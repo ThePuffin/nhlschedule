d7929130ff982c55f15d5adf178b90cfe5613074
ThePuffin <ballutvincent@gmail.com>
2022-07-23 18:24:49 +0200

    start project

---
+++ src/components/filter/filter.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Select from './select';
+
+class Filter extends React.Component {
+  render() {
+    return (
+      <div>
+        <Select />
+      </div>
+    );
+  }
+}
+
+export default Filter;
