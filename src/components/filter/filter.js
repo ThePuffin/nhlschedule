5a0d34028dae89d10ba8e3d9c241229dfa262280
ThePuffin <ballutvincent@gmail.com>
2022-07-24 11:55:02 +0200

    add a button + and select for team

--- src/components/filter/filter.js
+++ src/components/filter/filter.js
@@ -6,6 +6,10 @@ class Filter extends React.Component {
   render() {
     return (
       <div>
+        <a class="btn-floating btn-large waves-effect waves-light red">
+          <i class="material-icons">add</i>
+        </a>
+
         <Select />
       </div>
     );
--- src/components/filter/select.js
+++ src/components/filter/select.js
@@ -1,8 +1,28 @@
+import M from 'materialize-css';
 import React from 'react';
 
 class Select extends React.Component {
+  componentDidMount() {
+    M.AutoInit();
+  }
+  componentDidUpdate() {
+    M.AutoInit();
+  }
   render() {
-    return <div>Select</div>;
+    return (
+      <div>
+        <div className="input-field col s12">
+          <select defaultValue="">
+            <option value="" disabled>
+              Choose a team
+            </option>
+            <option value="1">Option 1</option>
+            <option value="2">Option 2</option>
+            <option value="3">Option 3</option>
+          </select>
+        </div>
+      </div>
+    );
   }
 }
 
