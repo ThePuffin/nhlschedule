4e5af3262fd1cc380c90343a348927db7082c4b5
ThePuffin <ballutvincent@gmail.com>
2022-07-24 11:11:35 +0200

    use typescript on project

---
+++ src/components/header/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Filter extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>NHL schedule</h1>
+      </div>
+    );
+  }
+}
+
+export default Filter;
