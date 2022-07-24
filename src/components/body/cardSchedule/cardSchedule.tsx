f014bf9bc82aebf0e3407e618e06168e18120ae3
ThePuffin <ballutvincent@gmail.com>
2022-07-24 18:22:17 +0200

    show all event in a card

--- src/components/body/cardSchedule/cardSchedule.tsx
+++ src/components/body/cardSchedule/cardSchedule.tsx
@@ -1,33 +1,57 @@
+import axios from 'axios';
 import M from 'materialize-css';
 import React from 'react';
 
 const moment = require('moment');
 
 class CardSchedule extends React.Component {
-  componentDidMount() {
+  state = {
+    teamDates: [],
+  };
+  async componentDidMount() {
     M.AutoInit();
+    try {
+      const resDate = await axios.get(
+        ` https://statsapi.web.nhl.com/api/v1/schedule?site=fr_nhl&startDate=2022-10-01&endDate=2022-10-30&teamId=1`
+      );
+      const teamDates = resDate.data.dates.map((date) => {
+        date.games = date.games[0];
+        return date;
+      });
+
+      this.setState({ teamDates });
+    } catch (error) {
+      console.error({ error });
+    }
   }
   componentDidUpdate() {
     M.AutoInit();
+    console.log(this.state.teamDates);
   }
   render() {
-    return (
-      <div className="input-field col s12">
-        <div className="row">
-          <div className="col s12 m6">
-            <div className="card blue-grey darken-1">
-              <div className="card-content white-text">
-                <span className="card-title">Card Title</span>
-                <p>
-                  I am a very simple card. I am good at containing small bits of information. I am convenient because I
-                  require little markup to use effectively.
-                </p>
+    if (this.state.teamDates.length) {
+      return (
+        <div className="input-field col s12">
+          {this.state.teamDates.map((teamDate) => (
+            <div className="row">
+              <div className="col s12 m6">
+                <div className="card blue-grey darken-1">
+                  <div className="card-content white-text">
+                    <span className="card-title">{teamDate.date}</span>
+                    <p>
+                      "{teamDate.games.teams.away.team.name}" VS "{teamDate.games.teams.home.team.name}"
+                    </p>
+                    <em>at : "{teamDate.games.venue.name}"</em>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
+          ))}
         </div>
-      </div>
-    );
+      );
+    } else {
+      return <p>no date</p>;
+    }
   }
 }
 
--- src/components/body/filter/select.tsx
+++ src/components/body/filter/select.tsx
@@ -11,7 +11,6 @@ class Select extends React.Component {
     try {
       const resTeams = await axios.get(`https://statsapi.web.nhl.com/api/v1/teams`);
       this.setState({ teams: resTeams.data.teams });
-      console.log(this.state.teams[0]);
     } catch (error) {
       console.error({ error });
     }
