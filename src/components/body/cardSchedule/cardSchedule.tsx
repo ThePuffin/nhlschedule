0f45f65c1862170b181d6e36f3bc3807baba61f5
ThePuffin <ballutvincent@gmail.com>
2022-07-31 10:20:39 +0200

    remove useless code

--- src/components/body/cardSchedule/cardSchedule.tsx
+++ src/components/body/cardSchedule/cardSchedule.tsx
@@ -1,35 +1,3 @@
-import M from 'materialize-css';
-import React from 'react';
-
-class CardScheduleClass extends React.Component {
-  async componentDidMount() {
-    M.AutoInit();
-  }
-  componentDidUpdate() {
-    M.AutoInit();
-  }
-  render() {
-    return (
-      <div>
-        <p>card</p>
-      </div>
-      // <div className="row">
-      //   <div className="col 12">
-      //     <div className="card blue-grey darken-1">
-      //       <div className="card-content white-text">
-      //         <span className="card-title">{teamDate.gameDate}</span>
-      //         <p>
-      //           "{teamDate.awayTeam}" VS "{teamDate.homeTeam}"
-      //         </p>
-      //         <em>at : "{teamDate.arenaName}"</em>
-      //       </div>
-      //     </div>
-      //   </div>
-      // </div>
-    );
-  }
-}
-
 const CardSchedule = (props) => {
   const teamDate = props.teamDate;
 
