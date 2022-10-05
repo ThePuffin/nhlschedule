2d67e9cd5475144f151c240e8f8b4ddfcc86755a
ThePuffin <ballutvincent@gmail.com>
2022-10-05 20:37:36 +0200

    only select 5 teams and not dupplicate day

--- src/components/body/body.tsx
+++ src/components/body/body.tsx
@@ -114,7 +114,11 @@ class Body extends React.Component {
       if (existingGame) {
         this.removeSelectedGame(teamData);
       } else {
+        gameSelected = gameSelected.filter((game) => game.gameDate !== teamData.gameDate);
         gameSelected.push(teamData);
+        if (gameSelected.length > maxTeamToSelect) {
+          gameSelected.shift();
+        }
 
         gameSelected.sort((a, b) => {
           return a.timestampDate - b.timestampDate;
